39f8d7a76569e9991adc3f98c46c9148c87ca24a
Fabio <fabiovlas@gmail.com>
2017-02-16 14:49:30 -0200

    Tinha bug

--- LabProg1/Revisao_C/ordena.cpp
+++ LabProg1/Revisao_C/ordena.cpp
@@ -5,12 +5,16 @@ int main()
 {
 	// Método bubblesort(?)
 	int *a,n,b;
+	
 	cout << "Entre com n: " << endl;
 	cin >> n;
+	
 	a = (int *) malloc(n* (sizeof(int)));
+	
 	for(int i=0;i<n;i++) scanf("%d",&a[i]);
+
 	for (int i=0;i<n;i++)
-		for (int j=0;j<i;j++)
+		for (int j=0;j<n-1;j++)
 		{
 			if (a[j+1] < a[j])
 			{
@@ -19,7 +23,10 @@ int main()
 				a[j+1] = b;
 			}
 		}
+	
 	for (int i=0;i<n;i++) cout << a[i] << " ";
+	
 	free(a);
-	cout << endl;
+	cout << endl; //a[i] = p[1]
+	// nome = &nome[0] p[i] = p+i 
 }
